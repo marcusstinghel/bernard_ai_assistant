0482192f6851cdebbc35b81d35f2cd4899dda81f
Marcus Stinghel <vini.o.stinghel@gmail.com>
2024-10-22 02:51:07 -0300

    UPDATED - Added new attributes [content.py]

--- models/content.py
+++ models/content.py
@@ -2,8 +2,8 @@ from pydantic import BaseModel, Field
 
 
 class Content(BaseModel):
-    question: str
-    customer_uuid: str
-    user_uuid: str
-    message: str = Field(default=None, alias='message')
+    question: str = Field(default=None, alias='question')
+    customer_uuid: str = Field(default=None, alias='customer_uuid')
+    user_uuid: str = Field(default=None, alias='user_uuid')
+    message_uuid: str = Field(default=None, alias='message_uuid')
     is_solved: bool = Field(default=None, alias='is_solved')
