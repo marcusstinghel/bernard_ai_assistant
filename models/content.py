0cd4347261e661a79949656a5c82b087dc9a3f0f
Marcus Stinghel <vini.o.stinghel@gmail.com>
2024-10-22 02:12:30 -0300

    UPDATED - Added attributes [content.py]

--- models/content.py
+++ models/content.py
@@ -1,7 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class Content(BaseModel):
     question: str
     customer_uuid: str
     user_uuid: str
+    message: str = Field(default=None, alias='message')
+    is_solved: bool = Field(default=None, alias='is_solved')
